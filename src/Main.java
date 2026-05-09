68948c01eeaec43f8f8ca0e6da95919b2b4cb6df
Nati200 <yonimy200@gamil.com>
2026-05-09 22:24:04 +0300

    add src files

---
+++ src/Main.java
@@ -0,0 +1,90 @@
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    static int balance = 0;
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+
+        int numOfDice;
+        int total = 0;
+
+        System.out.print("Enter the # of die you want to roll: ");
+        numOfDice = scanner.nextInt();
+       if(numOfDice > 0){
+           for(int i = 0; i < numOfDice; i++){
+               int roll = random.nextInt(1, 7);
+               printDie(roll);
+               System.out.println("you have rolled: " + roll);
+               total += roll;
+           }
+           System.out.println("the total will be: " + total);
+       }else{
+           System.out.println("you number of roll must be greater than 0");
+       }
+        scanner.close();
+    }
+    static void printDie(int roll){
+
+        String dice1
+                = """
+                -------
+                |     |
+                |  ●  |
+                |     |
+                -------
+                """;
+        String dice2
+                = """
+                -------
+                |●    |
+                |     |
+                |    ●|
+                -------
+                """;
+        String dice3
+                = """
+                -------
+                |●    |
+                |  ●  |
+                |    ●|
+                -------
+                """;
+        String dice4
+                = """
+                -------
+                |●   ●|
+                |     |
+                |●   ●|
+                -------
+                """;
+        String dice5
+                = """
+                -------
+                |●   ●|
+                |  ●  |
+                |●   ●|
+                -------
+                """;
+        String dice6
+                = """
+                -------
+                |●   ●|
+                |●   ●|
+                |●   ●|
+                -------
+                """;
+
+        switch (roll){
+            case 1 -> System.out.println(dice1);
+            case 2 -> System.out.println(dice2);
+            case 3 -> System.out.println(dice3);
+            case 4 -> System.out.println(dice4);
+            case 5 -> System.out.println(dice5);
+            case 6 -> System.out.println(dice6);
+            default -> System.out.println("guess we never know b!tch");
+    }
+    }
+}
---
+++ src/README.md
@@ -0,0 +1,23 @@
+Dice Roller Game 🎲
+
+A simple Java console application that simulates rolling one or more dice using random numbers and ASCII art.
+
+Features
+* Roll multiple dice at once
+* Random dice values from 1–6
+* ASCII art display for each die
+* Calculates the total of all rolls
+* Input validation for number of dice
+
+Technologies Used
+* Java 
+* Scanner 
+* Random 
+* switch expressions 
+* Methods
+
+How It Works
+1) User enters how many dice they want to roll. 
+2) The program generates random numbers between 1 and 6. 
+3) Each dice face is displayed using ASCII art. 
+4) The total value of all dice is shown at the end.
